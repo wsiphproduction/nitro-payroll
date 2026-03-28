afde0be5764c532c1fd0c9bc5263211650fb574c
randycph <randycph@gmail.com>
2026-03-28 10:52:51 +0800

    Update SyncEmployeesJob.php

--- app/Jobs/SyncEmployeesJob.php
+++ app/Jobs/SyncEmployeesJob.php
@@ -34,84 +34,100 @@ class SyncEmployeesJob implements ShouldQueue
             $payrollPassword = config('database.connections.payroll.password');
 
             $serverName = $payrollHost;
-            $connectionInfo = array( "Database"=>$payrollDatabase, "UID"=>$payrollUsername, "PWD"=>$payrollPassword);
-            $conn_payroll = sqlsrv_connect( $serverName, $connectionInfo);
+            $connectionInfo = array("Database" => $payrollDatabase, "UID" => $payrollUsername, "PWD" => $payrollPassword);
+            $conn_payroll = sqlsrv_connect($serverName, $connectionInfo);
 
             $hrisHost = config('database.connections.hris.host');
             $hrisDatabase = config('database.connections.hris.database');
             $hrisUsername = config('database.connections.hris.username');
             $hrisPassword = config('database.connections.hris.password');
 
-            $connectionInfosp = array( "Database"=>$hrisDatabase, "UID"=>$hrisUsername, "PWD"=>$hrisPassword);
-            $conn_hris = sqlsrv_connect( $hrisHost, $connectionInfosp);
+            $connectionInfosp = array("Database" => $hrisDatabase, "UID" => $hrisUsername, "PWD" => $hrisPassword);
+            $conn_hris = sqlsrv_connect($hrisHost, $connectionInfosp);
 
 
             //upload new employees from hris to payroll
             $dd = sqlsrv_query($conn_hris, "select * from users");
-            while($d=sqlsrv_fetch_array($dd)){
-            $qry="";
+            while ($d = sqlsrv_fetch_array($dd)) {
+                $qry = "";
                 $check = sqlsrv_fetch_array(
-                    sqlsrv_query($conn_payroll,"select id, hris_ref_id from users where hris_ref_id='".$d['id']."'")
+                    sqlsrv_query($conn_payroll, "select id, hris_ref_id from users where hris_ref_id='" . $d['id'] . "'")
                 );
-                
-                if(!$check){
-                $if="(";
-                $iv="(";
-                $cc = sqlsrv_query($conn_payroll,"sp_columns users");
-                while($c = sqlsrv_fetch_array($cc)){
 
-                $col =  $c['COLUMN_NAME']; 
-                $not_included = ['id','hdmf_ee','hdmf_er','company_branch_site_id','section_id','hris_ref_id'];
-            
-                if (!in_array($col, $not_included)) {
-                    $if.=$c['COLUMN_NAME'].",";
+                $dept = sqlsrv_fetch_array(sqlsrv_query($conn_hris, "
+                    SELECT TOP 1 department_id
+                    FROM employments
+                    WHERE user_id = '".$d['id']."'
+                    ORDER BY id DESC
+                "));
 
-                    if($c['TYPE_NAME'] == 'date'){
-                    if(isset($d[$col])){
-                    $iv.="'".$d[$col]->format('Y-m-d')."',";
+                $department_id = $dept ? $dept['department_id'] : 0;
+
+                if (!$check) {
+                    $if = "(";
+                    $iv = "(";
+                    $cc = sqlsrv_query($conn_payroll, "sp_columns users");
+                    while ($c = sqlsrv_fetch_array($cc)) {
+
+                        $col =  $c['COLUMN_NAME'];
+                        $not_included = ['id', 'hdmf_ee', 'hdmf_er', 'company_branch_site_id', 'section_id', 'hris_ref_id'];
+
+                        if (!in_array($col, $not_included)) {
+                            $if .= $c['COLUMN_NAME'] . ",";
+
+                            if ($c['TYPE_NAME'] == 'date') {
+                                if (isset($d[$col])) {
+                                    $iv .= "'" . $d[$col]->format('Y-m-d') . "',";
+                                } else {
+                                    $iv .= "'',";
+                                }
+                            } elseif ($c['TYPE_NAME'] == 'datetime') {
+                                if (isset($d[$col])) {
+                                    $iv .= "'" . $d[$col]->format('Y-m-d H:i:s') . "',";
+                                } else {
+                                    $iv .= "'',";
+                                }
+                            } else {
+                                $iv .= "'" . $d[$col] . "',";
+                            }
+                        }
                     }
-                    else{
-                    $iv.="'',";
+                    $iv .= "'" . $d['id'] . "',";
+                    $if .= "hris_ref_id" . ",";
+
+                    $iv = rtrim($iv, ",") . ")";
+                    $if = rtrim($if, ",") . ")";
+                    $qry = "insert into users " . $if . " values " . $iv;
+                    logger()->info('Insert Query: ' . $qry);
+                    $exec = sqlsrv_query($conn_payroll, $qry);
+
+                    if ($exec === false) {
+                        logger()->error('Insert failed: ' . print_r(sqlsrv_errors(), true));
+                    } else {
+                        logger()->info('Insert success for HRIS ID: ' . $d['id']);
                     }
-                
-                    }
-                    elseif($c['TYPE_NAME'] == 'datetime'){
-                    if(isset($d[$col])){
-                    $iv.="'".$d[$col]->format('Y-m-d H:i:s')."',";
-                    }
-                    else{
-                    $iv.="'',";
-                    }
-                    
-                    }
-                    else{
-                    $iv.="'".$d[$col]."',";
+                } else {
+                    $qryUpdate = "
+                        UPDATE users SET
+                            last_name = '".str_replace("'", "''", $d['last_name'])."',
+                            status = '".str_replace("'", "''", $d['status'])."',
+                            department_id = '".$department_id."',
+                            updated_at = GETDATE()
+                        WHERE hris_ref_id = '".$d['id']."'
+                    ";
+
+                    $execUpdate = sqlsrv_query($conn_payroll, $qryUpdate);
+
+                    if($execUpdate === false){
+                        logger()->error('Update failed: '.print_r(sqlsrv_errors(), true));
+                    } else {
+                        logger()->info('Update success for HRIS ID: '.$d['id']);
                     }
                 }
-                
-                }
-                $iv .= "'".$d['id']."',";
-                $if.="hris_ref_id".",";
-
-                $iv = rtrim($iv,",").")";
-                $if = rtrim($if,",").")";
-                $qry = "insert into users ".$if." values ".$iv;
-            logger()->info('Insert Query: '.$qry);    
-            $exec = sqlsrv_query($conn_payroll,$qry);
-
-            if($exec === false){
-                logger()->error('Insert failed: '.print_r(sqlsrv_errors(), true));
-            } else {
-                logger()->info('Insert success for HRIS ID: '.$d['id']);
             }
-
-            }
-            
-            }
-
 
             //update employment details of all employees
-            $q = sqlsrv_query($conn_hris,"select u.id as uid, u.employee_number, u.status,
+            $q = sqlsrv_query($conn_hris, "select u.id as uid, u.employee_number, u.status,
             di.id as division_id,di.name as division_name,
             de.id as department_idd, de.department_name as department_name,
             loc.id as location_id,loc.location as location_name,
@@ -131,14 +147,13 @@ class SyncEmployeesJob implements ShouldQueue
             order by u.id desc
             ");
 
-            while($r = sqlsrv_fetch_array($q)){     
-                $upd = sqlsrv_query($conn_payroll,"update users set department_id='".$r['department_idd']."', status='".$r['status']."'
-                job_title_id='".$r['position_idd']."',company_branch_id=3,section_id='".$r['section_idd']."',
-                company_branch_site_id=2,hdmf_ee=200,hdmf_er=200,employee_number='".$r['employee_number']."',salary_type='".$r['saltype']."',shortid='".$r['employee_number']."' where hris_ref_id='".$r['uid']."'");
-            
+            while ($r = sqlsrv_fetch_array($q)) {
+                $upd = sqlsrv_query($conn_payroll, "update users set department_id='" . $r['department_idd'] . "', status='" . $r['status'] . "'
+                job_title_id='" . $r['position_idd'] . "',company_branch_id=3,section_id='" . $r['section_idd'] . "',
+                company_branch_site_id=2,hdmf_ee=200,hdmf_er=200,employee_number='" . $r['employee_number'] . "',salary_type='" . $r['saltype'] . "',shortid='" . $r['employee_number'] . "' where hris_ref_id='" . $r['uid'] . "'");
             }
         } catch (\Throwable $e) {
-            \Log::error('SyncEmployeesJob failed: '.$e->getMessage());
+            \Log::error('SyncEmployeesJob failed: ' . $e->getMessage());
             throw $e; // let queue mark as failed
         }
     }

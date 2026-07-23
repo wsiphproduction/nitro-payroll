cae316580c7d4361bd896ee3399f933de76c0538
randycph <randycph@gmail.com>
2026-07-23 14:04:23 +0800

    remove TotalDeductions

--- app/Models/Reports.php
+++ app/Models/Reports.php
@@ -3136,7 +3136,7 @@ public function getPayrollRegisterApprovedReport($param){
 
             COALESCE(DeductionSummary.OtherDeduction,0) as OtherDeduction,
             
-            COALESCE(paytrnemp.TotalEEInsurancePremiums,0) + COALESCE(paytrnemp.TotalOtherDeductions,0) + COALESCE(paytrnemp.TotalDeductions,0) as TotalDeduction,
+            COALESCE(paytrnemp.TotalEEInsurancePremiums,0) + COALESCE(paytrnemp.TotalOtherDeductions,0) as TotalDeduction,
 
             COALESCE(paytrnemp.NetPay,0) as NetPay
             " . (!empty($dynamicColumns) ? "," . $dynamicColumns : "") . ",
@@ -3264,7 +3264,6 @@ public function getPayrollRegisterApprovedReport($param){
             SUM(
                 COALESCE(paytrnemp.TotalEEInsurancePremiums,0)
                 + COALESCE(paytrnemp.TotalOtherDeductions,0)
-                + COALESCE(paytrnemp.TotalDeductions,0)
             ) as TotalDeduction,
 
             SUM(COALESCE(paytrnemp.NetPay,0)) as NetPay
@@ -3534,7 +3533,7 @@ public function getPayrollRegisterPendingReport($param){
 
             COALESCE(DeductionSummary.OtherDeduction,0) as OtherDeduction,
 
-            COALESCE(paytrnemp.TotalEEInsurancePremiums,0) + COALESCE(paytrnemp.TotalOtherDeductions,0) + COALESCE(paytrnemp.TotalDeductions,0) as TotalDeduction,
+            COALESCE(paytrnemp.TotalEEInsurancePremiums,0) + COALESCE(paytrnemp.TotalOtherDeductions,0) as TotalDeduction,
 
             COALESCE(paytrnemp.NetPay,0) as NetPay,
 
@@ -3658,7 +3657,6 @@ public function getPayrollRegisterPendingReport($param){
             SUM(
                 COALESCE(paytrnemp.TotalEEInsurancePremiums,0)
                 + COALESCE(paytrnemp.TotalOtherDeductions,0)
-                + COALESCE(paytrnemp.TotalDeductions,0)
             ) as TotalDeduction,
 
             SUM(COALESCE(paytrnemp.NetPay,0)) as NetPay
